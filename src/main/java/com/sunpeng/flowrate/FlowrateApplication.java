543774f8bafaa34b6de78a0b59370077a7e3bd4f
Unknown <splovesx@qq.com>
2018-09-30 20:20:13 +0800

    打包测试
    打包测试

--- src/main/java/com/sunpeng/flowrate/FlowrateApplication.java
+++ src/main/java/com/sunpeng/flowrate/FlowrateApplication.java
@@ -2,11 +2,20 @@ package com.sunpeng.flowrate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class FlowrateApplication {
+public class FlowrateApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FlowrateApplication.class);
+    }
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(FlowrateApplication.class, args);
